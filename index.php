73c7c11a8747035481725d4725794a0ed9fab5da
Jacob Tran <jtran13@mail.greenriver.edu>
2018-01-11 17:21:51 -0500

    Adding views to php home

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ $f3 = Base::instance();
 
 //Define a default route
 $f3->route('GET /', function(){
-    echo "Pet Home";
+    $view = new View;
+    echo $view->render('views/home.html');
 });
 
 // Run fat free
